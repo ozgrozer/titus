d19d914d8a59948eedebc7f5e9763397d7f94d31
ozgrozer <ozgr@live.com>
2017-11-01 20:49:34 +0300

    Started to decode.

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 var hiddenMessage = function (opts) {
   var result
+  var characters = {
+    'a': '&#8288;',
+    'b': '&#8289;',
+    'c': '&#8290;',
+    'd': '&#8291;',
+    'e': 'e',
+    'f': 'f',
+    'g': 'g',
+    'h': 'h',
+    'i': 'i',
+    'j': 'j',
+    'k': 'k',
+    'l': 'l',
+    'm': 'm',
+    'n': 'n',
+    'o': 'o',
+    'p': 'p',
+    'q': 'q',
+    'r': 'r',
+    's': 's',
+    't': 't',
+    'u': 'u',
+    'v': 'v',
+    'w': 'w',
+    'x': 'x',
+    'y': 'y',
+    'z': 'z'
+  }
 
-  if (opts.text.length > 1) {
-    var characters = {
-      'a': '&#8288;',
-      'b': '&#8289;',
-      'c': '&#8290;',
-      'd': '&#8291;',
-      'e': '?',
-      'f': '?',
-      'g': '?',
-      'h': '?',
-      'i': '?',
-      'j': '?',
-      'k': '?',
-      'l': '?',
-      'm': '?',
-      'n': '?',
-      'o': '?',
-      'p': '?',
-      'q': '?',
-      'r': '?',
-      's': '?',
-      't': '?',
-      'u': '?',
-      'v': '?',
-      'w': '?',
-      'x': '?',
-      'y': '?',
-      'z': '?'
+  var firstLetterOfText = opts.text.substr(0, 1)
+  var restOfText = opts.text.substr(1)
+  var allReplace = function (text, obj) {
+    for (var key in obj) {
+      text = text.replace(new RegExp(key, 'g'), obj[key])
     }
+    return text
+  }
+  var hasUnicode = function (str) {
+    return /[^\u0000-\u00ff]/.test(str)
+  }
 
-    var firstLetterOfText = opts.text.substr(0, 1)
-    var restOfText = opts.text.substr(1)
-    var allReplace = function (text, obj) {
-      for (var key in obj) {
-        text = text.replace(new RegExp(key, 'g'), obj[key])
-      }
-      return text
+  if (opts.type === 'encode') {
+    if (opts.text.length > 1) {
+      result = firstLetterOfText + allReplace(opts.secretMessage, characters) + restOfText
+    } else {
+      result = 'Minimum 2 characters required.\nBecause secret message will be hidden in the visible letters.'
     }
-
-    result = firstLetterOfText + allReplace(opts.secretMessage, characters) + restOfText
-  } else {
-    result = 'Minimum 2 characters required.\nBecause secret message will be hidden in the visible letters.'
+  } else if (opts.type === 'decode') {
+    if (opts.text.length > 1) {
+      result = hasUnicode(opts.text)
+    } else {
+      result = 'Minimum 2 characters required.\nBecause secret message is hidden in the visible letters.'
+    }
   }
 
   return result
